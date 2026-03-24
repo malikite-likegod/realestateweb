52d4e0a2bead2d29dbf05a6f371e47a8d81b66e2
michael <blackiechan@gmail.com>
2026-03-23 20:26:27 -0400

    fix: trim ampre_compliance migration to only apply missing changes (syncType rename + member/office indexes)

--- prisma/migrations/20260323211131_ampre_compliance/migration.sql
+++ prisma/migrations/20260323211131_ampre_compliance/migration.sql
@@ -4,64 +4,6 @@
   - You are about to drop the column `resourceType` on the `reso_sync_logs` table. All the data in the column will be lost.
 
 */
--- AlterTable
-ALTER TABLE "reso_properties" ADD COLUMN "contractStatus" TEXT;
-ALTER TABLE "reso_properties" ADD COLUMN "documentsChangeTimestamp" DATETIME;
-ALTER TABLE "reso_properties" ADD COLUMN "listAgentFullName" TEXT;
-ALTER TABLE "reso_properties" ADD COLUMN "majorChangeTimestamp" DATETIME;
-ALTER TABLE "reso_properties" ADD COLUMN "mediaChangeTimestamp" DATETIME;
-ALTER TABLE "reso_properties" ADD COLUMN "mlsStatus" TEXT;
-ALTER TABLE "reso_properties" ADD COLUMN "photosChangeTimestamp" DATETIME;
-
--- CreateTable
-CREATE TABLE "ampre_sync_checkpoints" (
-    "syncType" TEXT NOT NULL PRIMARY KEY,
-    "lastTimestamp" DATETIME,
-    "lastKey" TEXT NOT NULL DEFAULT '0',
-    "updatedAt" DATETIME NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "reso_members" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "memberKey" TEXT NOT NULL,
-    "memberFullName" TEXT,
-    "memberEmail" TEXT,
-    "memberMobilePhone" TEXT,
-    "memberStatus" TEXT,
-    "officeKey" TEXT,
-    "officeName" TEXT,
-    "modificationTimestamp" DATETIME,
-    "photosChangeTimestamp" DATETIME,
-    "lastSyncedAt" DATETIME NOT NULL,
-    "rawJson" TEXT
-);
-
--- CreateTable
-CREATE TABLE "reso_offices" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "officeKey" TEXT NOT NULL,
-    "officeName" TEXT,
-    "officeEmail" TEXT,
-    "officePhone" TEXT,
-    "modificationTimestamp" DATETIME,
-    "photosChangeTimestamp" DATETIME,
-    "lastSyncedAt" DATETIME NOT NULL,
-    "rawJson" TEXT
-);
-
--- CreateTable
-CREATE TABLE "notifications" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "type" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "body" TEXT,
-    "contactId" TEXT,
-    "isRead" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "notifications_contactId_fkey" FOREIGN KEY ("contactId") REFERENCES "contacts" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
-
 -- RedefineTables
 PRAGMA defer_foreign_keys=ON;
 PRAGMA foreign_keys=OFF;
@@ -88,6 +30,3 @@ CREATE UNIQUE INDEX "reso_members_memberKey_key" ON "reso_members"("memberKey");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "reso_offices_officeKey_key" ON "reso_offices"("officeKey");
-
--- CreateIndex
-CREATE INDEX "notifications_isRead_createdAt_idx" ON "notifications"("isRead", "createdAt");
